ae78bcacba4e2b1c6b2b79b797728203e27cd7dd
YA <YA-androidapp@users.noreply.github.com>
2020-11-28 01:24:39 +0900

    minor updates

--- Replace-Href/replace-href.js
+++ Replace-Href/replace-href.js
@@ -4,27 +4,10 @@ var whitelist_href_urls = ['www.google.com', /^\//ig];
 
 
 var remove_query = function (str) {
-    return str ? str.replace(/\?[^?]+$/gi, '?REPLACED') : '';
+    return str ? str.replace(/\?[^?]+$/gi, '#REPLACED') : '';
 };
 
-
-var location_url = window.location.href;
-var queryString = window.location.search;
-
-console.log('location_url', location_url);
-
-
-// リンク元URLの判定
-var flag_black_page = false;
-blacklist_page_urls.some(function (bk_url) {
-    if (location_url.search(bk_url) > -1) {
-        console.log('black', location_url, bk_url);
-        flag_black_page = true;
-        return true;
-    }
-});
-
-if (flag_black_page) {
+var check = function () {
     var selects = document.getElementsByTagName('a');
     Array.prototype.forEach.call(selects, function (item) {
         href_url = item.getAttribute('href');
@@ -34,7 +17,7 @@ if (flag_black_page) {
                 var flag_white_href = false;
                 whitelist_href_urls.some(function (wh_url) {
                     if (href_url.search(wh_url) > -1) {
-                        console.log('white', location_url, href_url, wh_url);
+                        // console.log('white', location_url, href_url, wh_url);
                         flag_white_href = true;
                         return true;
                     }
@@ -50,4 +33,38 @@ if (flag_black_page) {
             }
         }
     });
+}
+
+
+var location_url = window.location.href;
+var queryString = window.location.search;
+// console.log('location_url: ', location_url);
+// console.log('queryString: ', queryString);
+
+
+// リンク元URLの判定
+var flag_black_page = false;
+blacklist_page_urls.some(function (bk_url) {
+    if (location_url.search(bk_url) > -1) {
+        flag_black_page = true;
+        return true;
+    }
+});
+
+if (flag_black_page) {
+    window.addEventListener('load', main, false);
+
+    function main(e) {
+        const jsInitCheckTimer = setInterval(jsLoaded, 1000);
+        function jsLoaded() {
+            var allLinks = document.getElementsByTagName('a');
+            var num = allLinks.length;
+            if (num < 5) {
+                clearInterval(jsInitCheckTimer);
+            } else {
+                check();
+                return;
+            }
+        }
+    }
 }
