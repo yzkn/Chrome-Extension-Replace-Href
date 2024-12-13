507a294691ce23840906c1472e0bd018667f51a0
YA <9039417+yzkn@users.noreply.github.com>
2024-12-13 12:44:55 +0900

    Delete background.js

--- Replace-Href/background.js
+++
@@ -1,8 +0,0 @@
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  if (tab.url.indexOf('http') > -1) {
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      files: ['replace-href.js'],
-    });
-  }
-});
